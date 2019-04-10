e6deb6069568208fde032b451a832b1e6151a4fa
jefshi <jefshi@yeah.net>
2019-04-10 17:44:11 +0800

    lib-adapter-recyclerview

--- lib-adapter-recyclerview/src/main/java/com/csp/adapter/recyclerview/SingleAdapter.java
+++ lib-adapter-recyclerview/src/main/java/com/csp/adapter/recyclerview/SingleAdapter.java
@@ -1,6 +1,7 @@
-package com.csp.library.android.base.adapter.recycle;
+package com.csp.adapter.recyclerview;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 
 import java.util.Collection;
 
@@ -13,6 +14,8 @@ import java.util.Collection;
  */
 @SuppressWarnings({"unused", "WeakerAccess"})
 public abstract class SingleAdapter<T> extends MultipleAdapter<T> {
+    private static final int DEFAULT_LAYOUT = 0;
+
     private int mLayoutId;
 
     public SingleAdapter(Context context, int layoutId) {
@@ -28,24 +31,44 @@ public abstract class SingleAdapter<T> extends MultipleAdapter<T> {
         addData(data, false);
     }
 
-    @Override
-    public int getItemCount() {
-        return mData.size();
+    public SingleAdapter(Context context, int layoutId, T[] data) {
+        super(context);
+
+        mLayoutId = layoutId;
+        addData(data, false);
     }
 
     @Override
     protected void addMultiViewFills() {
-        addViewFill(0, new IViewFill<T>() {
+        addViewFill(DEFAULT_LAYOUT, new IViewFill<T>() {
             @Override
             public int getLayoutId() {
                 return mLayoutId;
             }
 
             @Override
-            public void onBind(ViewHolder holder, T datum, int offset) {
-                SingleAdapter.this.onBind(holder, datum, offset);
+            public void onBind(ViewHolder holder, T datum, Object extra, int position) {
+                SingleAdapter.this.onBind(holder, datum, position);
             }
         });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return DEFAULT_LAYOUT;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        int viewType = getItemViewType(position);
+        IViewFill viewFill = getViewFill(viewType);
+        viewFill.onBind(holder, mData.get(position), null, position);
     }
 
     /**
@@ -56,7 +79,7 @@ public abstract class SingleAdapter<T> extends MultipleAdapter<T> {
     }
 
     /**
-     * {@link IViewFill#onBind(ViewHolder, Object, int)}
+     * {@link IViewFill#onBind(ViewHolder, Object, Object, int)}
      *
      * @param position 对应数据在列表中的位置
      */
